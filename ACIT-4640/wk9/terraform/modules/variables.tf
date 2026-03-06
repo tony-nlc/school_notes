0f44ba1eb45ec93d01e6becd30b8ec4890a1ba27
Tony <ngai.lam.chou@gmail.com>
2026-03-06 14:40:23 -0800

    vault backup: 2026-03-06 14:40:23

--- ACIT-4640/wk9/terraform/modules/variables.tf
+++ ACIT-4640/wk9/terraform/modules/variables.tf
@@ -1,16 +1,3 @@
-var "aws_instance" "web" {
-  ami                    = data.aws_ami.ansible-name.id
-  instance_type          = "t3.micro"
-  key_name               = "aws-4640"
-  vpc_security_group_ids = [aws_security_group.web.id]
-  subnet_id              = aws_subnet.web.id
-
-  tags = {
-    Name = "Web"
-  }
-}
-
-
 variable "project_name" {
     description = "Project Name"
     type = string
